0d3d5a8c6d5376a3696e3a0e369e487dcf788374
Antonio Llamas <antoniollamasln@gmail.com>
2023-10-26 16:14:18 +0200

    add update server.js

--- server.js
+++ server.js
@@ -2,38 +2,15 @@ const express = require('express')
 
 const app = express()
 
-// configura un middleware para parsear las entradas con body json y las coloca en req.body
 app.use(express.json())
 
 const db = { tasks: [] }
 
-app.get('/tasks', (req, res) => {
-	res.json(db.tasks)
-})
-app.get('/tasks/:taskId', (req, res) => {
-	console.log(req.params)
+app.get('/tasks', (req, res) => {})
+app.get('/tasks/:taskId', (req, res) => {})
 
-	db.tasks.push(req.params.taskId)
-
-	console.log(db.tasks)
-
-	res.send('tu task')
-})
-
-app.post('/tasks', (req, res) => {
-	console.log(req.body)
-})
+app.post('/tasks', (req, res) => {})
 app.put('/tasks/:taskId', (req, res) => {})
 app.delete('/tasks/:taskId', (req, res) => {})
 
 app.listen(4001, () => console.log('Server on..'))
-
-// put y delete (taskId)
-// Para identificar el recurso enviamos el input por el método params
-
-// req.params = {}
-
-// put y post (payload)
-// Para enviar la data del nuevo recursos o los cambios del recurso enviamos el input por el metodo body
-
-// req.body = {}
